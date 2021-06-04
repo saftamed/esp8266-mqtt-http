0a72893f7aca0e92e522fe34514990a50a8e5115
saftamed <saftanova@gmail.com>
2021-06-04 19:06:00 +0200

    5.0.0

--- sever+http+mqtt.ino
+++ sever+http+mqtt.ino
@@ -4,6 +4,15 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
+
+typedef struct
+  {
+      int pin;
+      int value;
+      String action;
+  }  input;
+
+input record[8];
 //-------- Customise these values -----------
 const char* token  = "4561"; 
 // const char* password = "u9pHINUEal";
@@ -27,19 +36,48 @@ char clientId[] = "safta001";
 
 
 bool pwd = true;
-
+int ii =0;
 WiFiClient wifiClient;
+int getIndex(int pin){
+   for(int i=0;i<ii;i++){
+     if(pin == record[i].pin)
+      return i;
+   }
+   return ii++;
+}
 void callback(char* topic, byte* payload, unsigned int payloadLength) {
   JSONVar myObject = JSON.parse((char *)payload);
           if (JSON.typeof(myObject) == "undefined") {
         Serial.println("Parsing input failed!");
         return;
       }
-        Serial.println((int)myObject["value"]);
-  if((String)((const char*)myObject["action"])=="D"){
+       Serial.println(myObject);
+
+        if((String)((const char*)myObject["action"])=="D"){
 
           pinMode((int)myObject["pin"],OUTPUT);
           digitalWrite((int)myObject["pin"],!(int)myObject["value"]);
+        }else  if((String)((const char*)myObject["action"])=="P"){
+                  String s =(const char*) myObject["value"];
+        //s.trim();
+         Serial.println(s.toInt()+2);
+             analogWrite((int)myObject["pin"],map( s.toInt(), 0, 100, 0, 1023));
+        }else  if((String)((const char*)myObject["action"])=="A"){
+
+           String s =(const char*) myObject["pin"];
+              int ss =getIndex(s.toInt());
+            Serial.println((int)myObject["pin"]);
+             Serial.println(s.toInt());
+              record[ss]={ s.toInt(),record[ss].value, "A"};
+              
+        }else  if((String)((const char*)myObject["action"])=="I"){
+           String s =(const char*) myObject["pin"];
+           
+            pinMode(s.toInt() ,INPUT);
+           int ss =getIndex(s.toInt());
+            Serial.println((int)myObject["pin"]);
+             Serial.println(s.toInt());
+              record[ss]={ s.toInt(),record[ss].value, "I"};
         }
 
 
@@ -56,11 +94,11 @@ String action = doc["action"];
 }
 PubSubClient client(serverr, 1883, callback, wifiClient);
 ESP8266WebServer server(80);
-int publishInterval = 5000; // 5 seconds//Send adc every 5sc
-long lastPublishMillis;
+int publishInterval = 1500; 
+long lastPublishMillis=0;
 
 void handleRoot() {
-  String response =F("<style>.form input[type=password],.form select{width:100%;padding:12px 20px;margin:8px 0;display:inline-block;border:1px solid #ccc;border-radius:4px;box-sizing:border-box}.form input[type=submit]{width:100%;background-color:#4caf50;color:white;padding:14px 20px;margin:8px 0;border:0;border-radius:4px;cursor:pointer}.form input[type=submit]:hover{background-color:#45a049}.form{border-radius:5px;background-color:#f2f2f2;padding:20px}</style><form class='form' action=\"update\"><select name=\"ssid\">");
+  String response ="";//F("<style>.form input[type=password],.form select{width:100%;padding:12px 20px;margin:8px 0;display:inline-block;border:1px solid #ccc;border-radius:4px;box-sizing:border-box}.form input[type=submit]{width:100%;background-color:#4caf50;color:white;padding:14px 20px;margin:8px 0;border:0;border-radius:4px;cursor:pointer}.form input[type=submit]:hover{background-color:#45a049}.form{border-radius:5px;background-color:#f2f2f2;padding:20px}</style><form class='form' action=\"update\"><select name=\"ssid\">");
   byte numSsid = WiFi.scanNetworks();
 
   for (int thisNet = 0; thisNet<numSsid; thisNet++) {
@@ -99,28 +137,28 @@ void serveForPwd(){
 void setup() {
 
   Serial.begin(9600); Serial.println(topic);
-
+/*
   
-//   serveForPwd();
-//   while(pwd){
-//       server.handleClient();
-//   }
-// WiFi.softAPdisconnect (true);
+  serveForPwd();
+  while(pwd){
+      server.handleClient();
+  }
+WiFi.softAPdisconnect (true);
 
-
+*/
   wifiConnect();
   delay(2000);
  httpGet();
   mqttConnect();
+  CheckData();
+  lastPublishMillis = millis();
 }
 
 void loop() {
-  /*if (millis() - lastPublishMillis > publishInterval) {
-    publishData();
+  if (millis() - lastPublishMillis > publishInterval) {
+    CheckData();
     lastPublishMillis = millis();
-    //delay(100);
-    //digitalWrite(2,LOW);
-  }*/
+  }
 
   if (!client.loop()) {
     mqttConnect();
@@ -160,14 +198,14 @@ void mqttConnect() {
   }
 }
 
-
-void publishData() {
-  // read the input on analog pin 0:
-  int sensorValue = 100;
-
-  String payload = "{\"d\":{\"adc\":";
-  payload += String(sensorValue, DEC);
-  payload += "}}";
+void sendData(input r){
+  String payload = "{\"action\":\"";
+  payload += r.action;
+  payload += "\",\"pin\":\"";
+  payload += String(r.pin, DEC);
+  payload += "\",\"value\":";
+  payload += String(r.value, DEC);
+  payload += "}";
 
   Serial.print("Sending payload: "); Serial.println(payload);
 
@@ -177,6 +215,32 @@ void publishData() {
   } else {
     Serial.println("Publish FAILED");
   }
+}
+void CheckData() {
+  // read the input on analog pin 0:
+
+  for(int i=0;i<ii;i++){
+    int v = 0;
+    if(record[i].action == "A"){
+      v= map(analogRead(record[i].pin),0,1023,0,100);
+      if(v >record[i].value + 5 || v<record[i].value -5){
+        record[i].value = v;
+        sendData(record[i]);
+      }
+    }else if(record[i].action == "I"){
+
+      v= digitalRead(record[i].pin);
+
+           
+      if(v != record[i].value){
+        record[i] = {  record[i].pin,v,"I"};
+        sendData(record[i]);
+         Serial.println(record[i].value);
+      }
+
+    }
+  }
+ 
 }
 
 void httpGet(){
@@ -203,16 +267,40 @@ void httpGet(){
 
 
 
-  Serial.println(myObject.length());
+    int ccc = myObject.length();
 
+      for (int i=ccc-1; i>=0; i--) {
+        String ss = (String)((const char*)myObject[i]["action"]);
+      Serial.println(myObject);
+       Serial.println(myObject);
+       JSONVar obj = myObject[i];
+        if(ss=="D"){
+            Serial.println("hhhhhhhhhh");
 
-      for (int i=0; i<myObject.length(); i++) {
-      
-            Serial.println((int)myObject[i]["value"]);
-        if((String)((const char*)myObject[i]["action"])=="D"){
+          pinMode((int)obj["pin"],OUTPUT);
+          digitalWrite((int)obj["pin"],!(int)obj["value"]);
 
-          pinMode((int)myObject[i]["pin"],OUTPUT);
-          digitalWrite((int)myObject[i]["pin"],!(int)myObject[i]["value"]);
+        }else if(ss=="P"){
+          Serial.println("2");
+                  String s =(const char*) obj["value"];
+        //s.trim();
+         Serial.println(s.toInt()+2);
+             analogWrite((int)obj["pin"],map( s.toInt(), 0, 100, 0, 1023));
+
+        }else if(ss=="A"){
+Serial.println("3");
+                String s =(const char*) obj["value"];
+              record[ii++]={ (int)obj["pin"], s.toInt(), "A"};
+                Serial.println("ii");
+                Serial.println(ii);
+
+        }else if(ss=="I"){
+          Serial.println("4");
+           pinMode((int)obj["pin"],INPUT);
+           String s =(const char*) obj["value"];
+              record[ii++]={ (int)obj["pin"], s.toInt(), "I"};
+        }else{
+          Serial.println("5");
         }
       
       }
